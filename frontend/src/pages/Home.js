7f53f20f14a324c57d8cfd5bbd406c0c5be06f53
MUKALLA PALLAVI <pallavimukalla@gmail.com>
2025-05-28 17:58:07 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default function Home() {
   return (
     <div className="container home">
-      <h1>Welcome to Your To-Do Application</h1>
+      <h1>Welcome to Your Task Manager Application</h1>
       <p>Manage your tasks easily and efficiently.</p>
       <div className="button-group">
         <Link to="/signup" className="btn btn-primary">Signup</Link>
