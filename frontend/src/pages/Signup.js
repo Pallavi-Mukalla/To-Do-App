c4b93f82aff2948589a6c2af7ed1fb11be2e14ed
MUKALLA PALLAVI <pallavimukalla@gmail.com>
2025-05-28 17:20:45 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -11,7 +11,7 @@ export default function Signup() {
     e.preventDefault();
     setError('');
 
-    fetch('http://localhost:5000/api/signup', {
+    fetch('https://to-do-app-9zxt.onrender.com/api/signup', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password }),
